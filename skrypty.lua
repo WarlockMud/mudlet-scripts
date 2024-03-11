977a3a969dbf1da9a0f3b4fa8db16b6264ea11fa
buczbucz <mateuszbukaty@gmail.com>
2024-03-12 00:35:30 +0100

    Zmiana wyświetlania cech.
    Zmiana wyświetlania cech.
    Version bump.

--- skrypty.lua
+++ skrypty.lua
@@ -1,4 +1,4 @@
-scripts = scripts or { ver = "2.1.15" }
+scripts = scripts or { ver = "2.1.16" }
 scripts["event_handlers"] = scripts["event_handlers"] or {}
 scripts["utils"] = {}
 scripts.character_name = ""
